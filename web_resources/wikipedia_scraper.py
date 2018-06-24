b14ea8a332d8aab8c433171ee3f5f5fc17b0b388
Christopher Brown <christopher.r.brown@aggiemail.usu.edu>
2018-06-24 14:02:57 -0600

    Replaced web scraping in favor of API calls.

--- web_resources/wikipedia_scraper.py
+++ web_resources/wikipedia_scraper.py
@@ -1,24 +1,8 @@
-import re
+import wikipedia
 
-import cleaner
-import scraper
 from annotations import return_errors_as_empty_string
 
 
-def scrape_wikipedia(item):
-    link = "https://en.wikipedia.org/wiki/" + item
-    page_text = scraper.get_website_text(link)
-    page_text = get_wikipedia_page_text(page_text)
-    page_text = cleaner.strip_html(page_text)
-    page_text = cleaner.strip_reference_blocks(page_text)
-    page_text = cleaner.strip_newlines(page_text)
-
-    return page_text
-
-
 @return_errors_as_empty_string
-def get_wikipedia_page_text(page_text):
-    regex = r"<p>(.+?)<div "
-    results = re.search(regex, page_text, re.S)
-
-    return results.group(1)
+def scrape_wikipedia(item):
+    return wikipedia.summary(item)
